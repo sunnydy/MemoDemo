490e0633fb0bfeef55b6811f8d3f2a78306dbce8
sunnydy <kmolim@gmail.com>
2018-06-09 22:37:28 +0900

    Server project initialized

---
+++ memodemo-server/src/test/java/kr/sunnyd/memodemo/MemodemoApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.sunnyd.memodemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemodemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
